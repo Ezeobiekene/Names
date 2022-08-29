a1b2af730a9c95311b2b886f3cafbe53ce7562ed
Ezeobiekene <95988883+Ezeobiekene@users.noreply.github.com>
2022-08-29 12:11:29 -0400

    Add files via upload

--- Names.java
+++ Names.java
@@ -1,14 +1,3 @@
-/**
- * Class: CIST 2371 Java Programming I
- * Term: Fall 2022
- * Instructor: Chris Bishop
- * Description: Solution to Lab #2
- * Author: Ekene Ezeobi
- * By turning in this code, I Pledge:
- * 1. That I have completed the programming assignment independently.
- * 2. I have not copied the code from a student or any source.
- * 3. I have not given my code to any student. *
- */
 
 import java.util.Scanner;
 
